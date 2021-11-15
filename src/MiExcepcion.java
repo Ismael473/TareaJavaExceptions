ac4fe1d989581ac0755c1ad9a4a202741d60f3c9
Ismael473 <62965318+Ismael473@users.noreply.github.com>
2021-11-14 23:39:17 -0600

    Excepción de Par en dado aleatorio

---
+++ src/MiExcepcion.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class MiExcepcion extends Exception{
+
+
+
+
+    public MiExcepcion(String mensaje){
+        super(mensaje);
+    }
+
+    public boolean dado(){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int valor = rand.nextInt(7);
+        System.out.println(valor);
+
+        boolean par;
+
+
+        try{
+            if (valor%2 == 0){
+                par = true;
+                throw new MiExcepcion("EL dado dio par "+valor);
+            }else{
+                par = false;
+            }
+        }catch (MiExcepcion mie){
+            System.err.println(mie);
+        }
+    }
+}
